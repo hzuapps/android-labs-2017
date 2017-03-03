293519c2c8abe3a3202bf5120cba055d7d747a7e
trjie <943614362@qq.com>
2017-03-03 16:55:22 +0800

    #1 #19 第一次实验

---
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903234/Net1414080903234Activity.java
@@ -0,0 +1,13 @@
+package com.example.hzxy.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Net1414080903234Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_net1414080903234);
+    }
+}
