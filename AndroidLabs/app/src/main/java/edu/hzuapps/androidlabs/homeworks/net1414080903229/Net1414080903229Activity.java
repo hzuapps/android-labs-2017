cccd8393e18f4cf1cee2875646c5a821c7eb9ac1
Feirty <1050416617@qq.com>
2017-03-03 19:43:08 +0800

    #1 #43 第一次作业

--- AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903229/Net1414080903229Activity.java
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903229/Net1414080903229Activity.java
@@ -3,7 +3,7 @@ package com.example.administrator.net1414080903229;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Net1414080903229 extends AppCompatActivity {
+public class Net1414080903229Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
