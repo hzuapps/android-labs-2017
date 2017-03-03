1aeb024d90e08846f0683d3549e2c620ebad8171
Paynehe <573769918@qq.com>
2017-03-03 21:36:58 +0800

    #1 #66 第一次作业

--- AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903103/net1414080903103Activity.java
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903103/net1414080903103Activity.java
@@ -1,3 +1,13 @@
-public class net1414080903103Activity {
-public static void main (String[] arys )
-{System.out.println("hello wold");}}
+package com.example.hepengji.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Net1414080903103Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_net1414080903103);
+    }
+}
