cdd01117f7b4105aeead0f1966caa0833d9d4fc6
LZR-PC <850048573@qq.com>
2017-03-03 19:15:52 +0800

    #1 #16 第一次作业

---
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903116/Net1414080903116Activity.java
@@ -0,0 +1,13 @@
+package com.example.lzr.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Net1414080903116Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.net1414080903116_activity);
+    }
+}
