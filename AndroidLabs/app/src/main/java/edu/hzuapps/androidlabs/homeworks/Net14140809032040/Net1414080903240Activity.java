9f5afc89b7481889fed72ecdaa67087592ed19b1
mokulai <1151311711@qq.com>
2017-03-03 20:43:15 +0800

    #1 #30 第一次作业

--- AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/Net14140809032040/Net1414080903240Activity.java
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/Net14140809032040/Net1414080903240Activity.java
@@ -1,4 +1,4 @@
-package com.example.administrator.myapplication;
+package androidlabs.hzuapps.edu.net1414080903120;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
