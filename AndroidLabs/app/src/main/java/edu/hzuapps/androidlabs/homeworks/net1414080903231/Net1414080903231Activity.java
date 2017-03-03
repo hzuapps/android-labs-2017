0018cd6d8a3617d08f20bc838dcfbbc7648c813e
ileyinan <905102629@qq.com>
2017-03-03 21:14:59 +0800

    #1 #26 第一次作业

--- AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903231/Net1414080903231Activity.java
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903231/Net1414080903231Activity.java
@@ -1,16 +1,13 @@
-package com.example.kali.net1414080903231;
+package edu.hzuapps.androidlabs.homeworks.net1414080903231activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 
-public class Net1414080903231 extends AppCompatActivity {
+public class Net1414080903231Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_net1414080903231);
     }
-
 }
