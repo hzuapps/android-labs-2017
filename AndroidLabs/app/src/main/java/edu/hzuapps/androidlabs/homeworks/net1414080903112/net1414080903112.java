127b9f33cc3d6784b92b07c37a38f1b2691b795d
wzk2333 <1017722647@qq.com>
2017-03-03 16:42:17 +0800

    #1 net 1414080903112
    第一次作业

---
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903112/net1414080903112.java
@@ -0,0 +1,13 @@
+package com.example.ohmygua.net1414080903112;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class net1414080903112 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_net1414080903112);
+    }
+}
