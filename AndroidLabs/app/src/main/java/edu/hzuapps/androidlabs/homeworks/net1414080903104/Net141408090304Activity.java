cb6d244c82472c881f5dc81dfd29b8cd1418e159
1414080903104 <937671117@qq.com>
2017-03-04 11:42:53 +0800

    #1 #22 第一次作业(修正版)

---
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903104/Net141408090304Activity.java
@@ -0,0 +1,15 @@
+package edu.hzuapps.myapplication.homework.Net1414080903104Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.hzuapps.myapplication.R;
+
+public class Net141408090304Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_net141408090304);
+    }
+}
