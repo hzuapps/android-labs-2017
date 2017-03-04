2dbcd0f1d6e4b7ced6b1526e5d981528fe0a15f1
net217ptz <254093288@qq.com>
2017-03-04 16:32:23 +0800

    #1 #92 第一次作业

---
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903217/net1414080903217Activity.java
@@ -0,0 +1,13 @@
+package com.example.mgb.firstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class net1414080903217Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_net1414080903217);
+    }
+}
