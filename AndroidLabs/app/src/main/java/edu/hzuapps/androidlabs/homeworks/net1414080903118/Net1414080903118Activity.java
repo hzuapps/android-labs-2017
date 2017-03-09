2aac71bd52c2d9230cf0f700e8e6090147c0b64f
unknown <7>
2017-03-09 10:32:01 +0800

    #1 #131 第一次作业

--- AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903118/Net1414080903118Activity.java
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903118/Net1414080903118Activity.java
@@ -1,7 +1,9 @@
-package edu.hzuapps.androidlabs;
+package edu.hzuapps.androidlabs.homeworks.net1414080903118;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import edu.hzuapps.androidlabs.R;
 
 public class Net1414080903118Activity extends AppCompatActivity {
 
