6583fdcd26d416f52c86452dbe3ab562cf1177a8
wangkaiqi <1211697101@qq.com>
2017-03-12 15:18:27 +0800

    #1 #32 第一次作业

--- AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903201/Net1414080903201Activity.java
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903201/Net1414080903201Activity.java
@@ -11,6 +11,5 @@ public class Net1414080903201Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_net1414080903201);
-
     }
 }
