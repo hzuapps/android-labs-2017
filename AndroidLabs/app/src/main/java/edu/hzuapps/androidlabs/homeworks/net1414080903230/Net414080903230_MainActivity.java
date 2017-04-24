274839a24e781830a84b2f6baab181d96ad70a85
shezhiming <505306252@qq.com>
2017-04-24 13:05:04 +0800

    #2 #15第二次实验

---
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903230/Net414080903230_MainActivity.java
@@ -0,0 +1,139 @@
+package edu.hzuapps.androidlabs.homework.net1414080903230.net1414080903230;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity; // 注意这里我们导入的V4的包，不要导成app的包了
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+/**
+ * 主页面内容
+ * Created by dm on 16-1-19.
+ */
+public class Net414080903230_MainActivity extends FragmentActivity implements View.OnClickListener {
+    // 初始化顶部栏显示
+    private ImageView titleLeftImv;
+    private TextView titleTv;
+    // 定义4个Fragment对象
+    private Net1414080903230_DiaryFragment fg1;
+    private Net1414080903230_UserFragment fg2;
+    // 帧布局对象，用来存放Fragment对象
+    private FrameLayout frameLayout;
+    // 定义每个选项中的相关控件
+    private RelativeLayout firstLayout;
+    private RelativeLayout secondLayout;
+    private ImageView firstImage;
+    private ImageView secondImage;
+    private TextView firstText;
+    private TextView secondText;
+    // 定义几个颜色
+    private int whirt = 0xFFFFFFFF;
+    private int gray = 0xFF7597B3;
+    private int dark = 0xff000000;
+    // 定义FragmentManager对象管理器
+    private FragmentManager fragmentManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_net1414080903230__main);
+        fragmentManager = getSupportFragmentManager();
+        initView(); // 初始化界面控件
+        setChioceItem(0); // 初始化页面加载时显示第一个选项卡
+    }
+    /**
+     * 初始化页面
+     */
+    private void initView() {
+// 初始化页面标题栏
+        titleLeftImv = (ImageView) findViewById(R.id.title_imv);
+        titleTv = (TextView) findViewById(R.id.title_text_tv);
+        titleTv.setText("首 页");
+// 初始化底部导航栏的控件
+        firstImage = (ImageView) findViewById(R.id.first_image);
+        secondImage = (ImageView) findViewById(R.id.second_image);
+        firstText = (TextView) findViewById(R.id.first_text);
+        secondText = (TextView) findViewById(R.id.second_text);
+        firstLayout = (RelativeLayout) findViewById(R.id.first_layout);
+        secondLayout = (RelativeLayout) findViewById(R.id.second_layout);
+        firstLayout.setOnClickListener(Net414080903230_MainActivity.this);
+        secondLayout.setOnClickListener(Net414080903230_MainActivity.this);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.first_layout:
+                setChioceItem(0);
+                break;
+            case R.id.second_layout:
+                setChioceItem(1);
+                break;
+
+        }
+    }
+    /**
+     * 设置点击选项卡的事件处理
+     *
+     * @param index 选项卡的标号：0, 1, 2, 3
+     */
+    private void setChioceItem(int index) {
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        clearChioce(); // 清空, 重置选项, 隐藏所有Fragment
+        hideFragments(fragmentTransaction);
+        switch (index) {
+            case 0:
+// firstImage.setImageResource(R.drawable.XXXX); 需要的话自行修改
+                firstText.setTextColor(dark);
+                firstLayout.setBackgroundColor(gray);
+// 如果fg1为空，则创建一个并添加到界面上
+                if (fg1 == null) {
+                    fg1 = new Net1414080903230_DiaryFragment();
+                    fragmentTransaction.add(R.id.content, fg1);
+                } else {
+// 如果不为空，则直接将它显示出来
+                    fragmentTransaction.show(fg1);
+                }
+                break;
+            case 1:
+// secondImage.setImageResource(R.drawable.XXXX);
+                secondText.setTextColor(dark);
+                secondLayout.setBackgroundColor(gray);
+                if (fg2 == null) {
+                    fg2 = new Net1414080903230_UserFragment();
+                    fragmentTransaction.add(R.id.content, fg2);
+                } else {
+                    fragmentTransaction.show(fg2);
+                }
+                break;
+
+        }
+        fragmentTransaction.commit(); // 提交
+    }
+    /**
+     * 当选中其中一个选项卡时，其他选项卡重置为默认
+     */
+    private void clearChioce() {
+// firstImage.setImageResource(R.drawable.XXX);
+        firstText.setTextColor(gray);
+        firstLayout.setBackgroundColor(whirt);
+// secondImage.setImageResource(R.drawable.XXX);
+        secondText.setTextColor(gray);
+        secondLayout.setBackgroundColor(whirt);
+    }
+    /**
+     * 隐藏Fragment
+     *
+     * @param fragmentTransaction
+     */
+    private void hideFragments(FragmentTransaction fragmentTransaction) {
+        if (fg1 != null) {
+            fragmentTransaction.hide(fg1);
+        }
+        if (fg2 != null) {
+            fragmentTransaction.hide(fg2);
+        }
+
+    }
+}
