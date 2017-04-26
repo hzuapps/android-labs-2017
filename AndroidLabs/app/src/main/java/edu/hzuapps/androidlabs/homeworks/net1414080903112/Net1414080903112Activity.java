409c14c85ce53bc1b019cd9d009d1f7c22a12c4f
wzk2333 <1017722647@qq.com>
2017-04-26 18:21:34 +0800

    #2 #16 第二次作业
    第二次实验

--- AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903112/Net1414080903112Activity.java
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903112/Net1414080903112Activity.java
@@ -1,8 +1,22 @@
-package edu.hzuapps.androidlabs.homeworks.Net1414080903112;
+package com.edu.hzuapps.androidlabs.net1414080903112;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.content.Intent;
+import android.view.View;//注意view的大小写
+import android.view.View.OnClickListener;
+import android.os.Bundle;
+import android.widget.Button;
 public class Net1414080903112Activity extends AppCompatActivity {
 
     @Override
