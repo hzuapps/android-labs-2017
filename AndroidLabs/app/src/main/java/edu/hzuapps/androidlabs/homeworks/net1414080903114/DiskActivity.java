d08a66a7c6d6aa1eac285a7f200695b1ce5ecd4b
QuanGuanQiao <1015567125@qq.com>
2017-04-26 17:51:39 +0800

    #2 #71 第二次实验

---
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903114/DiskActivity.java
@@ -0,0 +1,13 @@
+package edu.hzuapps.androidlabs.homeworks.net1414080903114;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DiskActivity extends Activity{
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_disk_net1414080903114);
+        //Log.i("OneActivity", "onCreate");
+    }
+
+}
