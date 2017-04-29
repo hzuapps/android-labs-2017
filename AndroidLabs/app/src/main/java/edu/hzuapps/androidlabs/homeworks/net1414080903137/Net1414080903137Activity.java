ffc7785b296a14b07847a9e9e4af5f908ff7cbf0
xuchunyu14 <1079661764@qq.com>
2017-04-29 22:31:37 +0800

    #2 #37 第二次作业

--- AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903137/Net1414080903137Activity.java
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903137/Net1414080903137Activity.java
@@ -1,15 +1,53 @@
 package edu.hzuapps.androidlabs.homworks.net1414080903137;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import edu.hzuapps.androidlabs.R;
 
 public class Net1414080903137Activity extends AppCompatActivity {
 
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_net1414080903137);
+        Button button1=(Button) findViewById(R.id.button);
+        Button button2=(Button) findViewById(R.id.button2);
+        Button button3=(Button)findViewById(R.id.button3);
+        Button button4=(Button)findViewById(R.id.button4);
+        button1.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent intent = new Intent(Net1414080903137Activity.this, Net1414080903137SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Net1414080903137Activity.this, Net1414080903137ThirdActivity.class);
+                startActivity(intent);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Net1414080903137Activity.this, Net1414080903137FourthActivity.class);
+                startActivity(intent);
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Net1414080903137Activity.this, Net1414080903137fFifthActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
---
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903137/Net1414080903137ThirdActivity.java
@@ -0,0 +1,17 @@
+package edu.hzuapps.androidlabs.homworks.net1414080903137;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import edu.hzuapps.androidlabs.R;
+
+public class Net1414080903137ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.Net1414080903137third);
+
+    }
+
+}
