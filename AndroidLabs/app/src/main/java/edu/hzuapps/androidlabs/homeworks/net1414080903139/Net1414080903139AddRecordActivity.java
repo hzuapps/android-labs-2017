1b7ef41d9e1fd8bf01d846c48beb7e8c7a7baabc
vwuyang <857728098@qq.com>
2017-04-30 14:50:25 +0800

    #2 #177 第二次作业

---
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903139/Net1414080903139AddRecordActivity.java
@@ -0,0 +1,48 @@
+package edu.hzuapps.androidlabs.net1414080903139;
+
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import edu.hzuapps.myapplication.R;
+
+/**
+ * Created by 阳飞 on 2017/4/29.
+ */
+
+public class Net1414080903139AddRecordActivity extends AppCompatActivity {
+
+    EditText etName;
+    EditText etBirthday;
+
+    Button btSubmit;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.net1414080903139activity02);
+
+        etBirthday= (EditText) findViewById(R.id.et_name);
+        etName= (EditText) findViewById(R.id.et_birthday);
+
+        btSubmit= (Button) findViewById(R.id.btn_submit);
+
+        btSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MySQLiteHelper helper=new MySQLiteHelper(Net1414080903139AddRecordActivity.this);
+                SQLiteDatabase db=helper.getWritableDatabase();
+                ContentValues values=new ContentValues();
+                values.put("name",etName.getText().toString().trim());
+                values.put("birthday",etBirthday.getText().toString().trim());
+                db.insert("birthday",null,values);
+                db.close();
+            }
+        });
+    }
+}
---
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903139/Net1414080903139MainActivity.java
@@ -0,0 +1,37 @@
+package edu.hzuapps.androidlabs.net1414080903139;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import edu.hzuapps.myapplication.R;
+
+public class Net1414080903139MainActivity extends AppCompatActivity {
+
+    Button btRecord;
+    Button btCheck;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.net1414080903139activity01);
+        btRecord= (Button) findViewById(R.id.bt_record_birthday);
+        btCheck= (Button) findViewById(R.id.bt_check_birthday);
+
+        btCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Net1414080903139MainActivity.this,Net1414080903139CheckActivity.class));
+            }
+        });
+
+        btRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Net1414080903139MainActivity.this,Net1414080903139AddRecordActivity.class));
+            }
+        });
+    }
+}
