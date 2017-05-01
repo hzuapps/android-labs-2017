3a860157ea22eceab8a94252a38e0b73aa6a531f
1414080903124 <1069322849@qq.com>
2017-05-02 00:07:37 +0800

    #2 #73 第二次作业

--- AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/Net1414080903124/Net1414080903124Activity.java
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/Net1414080903124/Net1414080903124Activity.java
@@ -1,7 +1,11 @@
-package edu.hzuapps.androidlabs.homworks.net1414080903124;
+package edu.hzuapps.androidlabs.homeworks.net1414080903124;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import edu.hzuapps.androidlabs.R;
 
 public class Net1414080903124Activity extends AppCompatActivity {
 
@@ -10,4 +14,17 @@ public class Net1414080903124Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_net1414080903124);
     }
+    public void click1(View view) {
+        Intent intent = new Intent(this, Activity02.class);
+        startActivity(intent);
+    }
+    public void click2(View view) {
+        Intent intent = new Intent(this, Activity03.class);
+        startActivity(intent);
+    }
+    public void click3(View view) {
+        Intent intent = new Intent(this, Activity04.class);
+        startActivity(intent);
+    }
 }
+
