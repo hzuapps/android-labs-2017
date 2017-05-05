bfd471ebf4eb3f35534f42aac2cbc9d100305532
linzihong888 <741637404@qq.com>
2017-05-05 15:24:45 +0800

    #2 #154 第二次作业

---
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903131/StudentBean.java
@@ -0,0 +1,54 @@
+package edu.hzuapps.androidlab.net1414080903131;
+
+/**
+ * Created by Administrator on 2017/4/21.
+ */
+
+public class StudentBean {
+
+    String id;
+    String name;
+    String sex;
+    String dormitory;
+    String clazz;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getDormitory() {
+        return dormitory;
+    }
+
+    public void setDormitory(String dormitory) {
+        this.dormitory = dormitory;
+    }
+
+    public String getClazz() {
+        return clazz;
+    }
+
+    public void setClazz(String clazz) {
+        this.clazz = clazz;
+    }
+}
