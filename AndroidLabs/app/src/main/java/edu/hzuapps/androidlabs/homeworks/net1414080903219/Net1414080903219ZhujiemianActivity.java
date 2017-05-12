038ae63456cfa00c1b85c5d18e0850d52ac91f8a
14w2mary <2934992529@qq.com>
2017-05-12 09:31:10 +0800

    第二次作业

--- AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903219/Net1414080903219ZhujiemianActivity.java
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903219/Net1414080903219ZhujiemianActivity.java
@@ -8,18 +8,18 @@ import android.widget.Button;
 
 import homeworks.androids.hzuapps.edu.application.R;
 
-public class ZhucaidanActivity extends AppCompatActivity {
+public class Net1414080903219ZhujiemianActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_zhucaidan);
+        setContentView(R.layout.zhujiemian_net1414080903219);
 
-        Button button1=(Button)findViewById(R.id.button_1);
-        button1.setOnClickListener(new View.OnClickListener(){
+        Button button1 = (Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                Intent intent=new Intent(ZhucaidanActivity.this,StudyActivity.class);
+            public void onClick(View v) {
+                Intent intent = new Intent(Net1414080903219ZhujiemianActivity.this, Net1414080903219StudyActivity.class);
                 startActivity(intent);
             }
         });
