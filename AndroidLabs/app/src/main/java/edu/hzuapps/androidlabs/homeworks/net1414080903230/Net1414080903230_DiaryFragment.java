ee9dac189a49baf0306fc6c7d92f4802dc9c5246
shezhiming <505306252@qq.com>
2017-05-12 20:21:38 +0800

    #4 #15 第四次实验

--- AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903230/Net1414080903230_DiaryFragment.java
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903230/Net1414080903230_DiaryFragment.java
@@ -15,8 +15,58 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class Net1414080903230_DiaryFragment extends Fragment {
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    private ListView list_show;
+
+
+    private View specItemView;
+
+    public Net1414080903230_DiaryFragment() {
+    }
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_net1414080903230__diary, container, false);
+        view.findViewById(R.id.btn_writediary).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Net1414080903230_WriteDiaryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        File sdpath = Environment.getExternalStorageDirectory();
+
+        //打开指定目录，显示项目说明书列表，供用户选择
+        File dir = new File(sdpath,"diarydata");
+        if(!dir.exists()){
+            Log.d("test1","tet");
+            dir.mkdirs();
+        }
+        //取出文件列表：
+        final File[] files = dir.listFiles();
+        try {
+
+            List<HashMap<String, Object>> specs = new ArrayList<HashMap<String, Object>>();
+            int seq = 0;
+            for (File spec : files) {
+                seq++;
+                HashMap<String, Object> hashMap = new HashMap<String, Object>();
+                hashMap.put("seq", seq);
+                hashMap.put("name", spec.getName());
+                Log.d("test",spec.getName());
+                specs.add(hashMap);
+            }
+
+
+            list_show = (ListView) view.findViewById(R.id.diary_list);
+
+            SimpleAdapter spa = new SimpleAdapter(getActivity(), specs, R.layout.spec_item_list_1414080903230,
+                    new String[]{"seq", "name"}, new int[]{R.id.seq, R.id.name});
+            list_show.setAdapter(spa);
+        }catch(NullPointerException e){
+
+
+        }
+
         return view;
     }
 }
--- AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903230/Net1414080903230_WriteDiaryActivity.java
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903230/Net1414080903230_WriteDiaryActivity.java
@@ -9,21 +9,56 @@ import android.view.View;
 
 public class Net1414080903230_WriteDiaryActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_net1414080903230__write_diary);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+    }
+
+    public void click(View view){
+        //get the diary string
+        TextView diary = (TextView) findViewById(R.id.atm_diary_content);
+        String diaryContent = diary.getText().toString();
+
+        //open the diarydata fold
+        //File yygypath = this.getFilesDir();
+        File sdpath = Environment.getExternalStorageDirectory();
+        //get the time and it will become the diary_name at this time
+        SimpleDateFormat    sDateFormat    =   new    SimpleDateFormat("yyyyMMddhhmmss");
+        String    date    =    sDateFormat.format(new    java.util.Date());
+        //add
+        //if(dir.exists()&&dir.canWrite()){
+            try {
+                File dir = new File(sdpath,"diarydata");
+                if(!dir.exists()){
+                    Log.d("file2",dir.getAbsolutePath());
+                    dir.mkdirs();
+                }
+
+                File newfile = new File(dir,date+".txt");
+                FileOutputStream out = new FileOutputStream(newfile);
+                //newfile.createNewFile();
+                //out = openFileOutput(dir.getAbsolutePath() + "/" + date+".txt", Context.MODE_PRIVATE);
+                //out = openFileOutput(newfile, Context.MODE_PRIVATE);
+                sDateFormat = new    SimpleDateFormat("yyyy-MM-dd  hh:mm:ss");
+                date = sDateFormat.format(new    java.util.Date());
+                date += "\n";
+                diaryContent += date;
+                out.write(diaryContent.getBytes("UTF-8"));
+                out.close();
+                final File[] files = dir.listFiles();
+                Log.d("file",Integer.toString(files.length));
+            }catch(Exception e){
+                e.printStackTrace();
             }
-        });
+
+       //}
+        Log.d("test","test");
+        Intent intent = new Intent(this,Net414080903230_MainActivity.class);
+        startActivity(intent);
+
     }
 
 }
