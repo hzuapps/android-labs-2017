b9b81f96174a1dd1fbac7433d42db34c92f7d515
14w2mary <2934992529@qq.com>
2017-05-12 09:37:41 +0800

    #3 #153 第三次作业

---
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903219/Net1414080903219Word.java
@@ -0,0 +1,9 @@
+package homeworks.androids.hzuapps.edu.application.net1414080903219;
+
+/**
+ * Created by mary on 2017/5/5.
+ */
+
+public class Net1414080903219Word {
+    public String name, meaning, status;
+}
