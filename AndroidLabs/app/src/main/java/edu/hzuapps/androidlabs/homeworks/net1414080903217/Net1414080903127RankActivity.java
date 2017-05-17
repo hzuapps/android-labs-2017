c19709537428a8744a1d8bd45bb6e608e0604b8a
hshaohua <945327536@qq.com>
2017-05-17 16:01:52 +0800

    #4 #28 第四次作业

---
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903217/Net1414080903127RankActivity.java
@@ -0,0 +1,13 @@
+package edu.hzuapps.androidlabs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Net1414080903127RankActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_net1414080903127_rank);
+    }
+}
