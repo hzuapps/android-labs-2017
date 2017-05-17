958af520c092b010ea450d23778015bb03ccb5eb
FYJ1995 <451940791@qq.com>
2017-05-17 22:54:03 +0800

    #1 #117 第二次作业

---
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903203/Net1414080903203Get.java
@@ -0,0 +1,32 @@
+package edu.hzuapps.androidlabs.homeworks.net1414080903203;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import edu.hzuapps.androidlabs.R;
+
+
+
+public class Net1414080903203Get extends AppCompatActivity {
+    private TextView tv_name,tv_password,tv_sex;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_net1414080903203get);
+        //获取Intent对象
+        Intent intent = getIntent();
+        //取出key对应的value值
+        String name = intent.getStringExtra("name");
+        String password = intent.getStringExtra("password");
+        String sex = intent.getStringExtra("sex");
+        tv_name = (TextView) findViewById(R.id.tv_name);
+        tv_password = (TextView) findViewById(R.id.tv_password);
+        tv_sex = (TextView) findViewById(R.id.tv_sex);
+        tv_name.setText("用户名： " + name);
+        tv_password.setText("密码： " + password);
+        tv_sex.setText("性别： " + sex);
+    }
+}
