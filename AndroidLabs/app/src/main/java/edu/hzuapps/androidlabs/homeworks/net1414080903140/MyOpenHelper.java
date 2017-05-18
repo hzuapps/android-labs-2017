a5aa145f8645c135b64fcc864fdd914b527aae34
666Ohayo <806123681@qq.com>
2017-05-18 23:11:54 +0800

    #4 #29 第四次作业

---
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903140/MyOpenHelper.java
@@ -0,0 +1,25 @@
+package edu.hzuapps.androidlabs.homeworks.net1414080903140;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Administrator on 2017/5/18.
+ */
+
+public class MyOpenHelper extends SQLiteOpenHelper {
+    public MyOpenHelper(Context context) {
+        super(context, "game", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table record(play_time varchar(20),score integer);");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
