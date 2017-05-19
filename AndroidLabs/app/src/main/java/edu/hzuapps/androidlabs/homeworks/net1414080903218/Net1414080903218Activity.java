ff80c820c5fcd01ef8235c8643b538c958fec1a3
zzh1414080903218 <1290040567@qq.com>
2017-05-19 21:16:40 +0800

    #4 #24 第四次实验

--- AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903218/Net1414080903218Activity.java
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903218/Net1414080903218Activity.java
@@ -1,26 +1,74 @@
 package edu.hzuapps.androidlabs.homeworks.net1414080903218;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class Net1414080903218Activity extends AppCompatActivity {
-
+    protected Intent IntentStartBoard;
+    protected String size;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_net1414080903218);
         Button ButtonPtoC=(Button) findViewById(R.id.PtoCButton);
+        Button ButtonPtoP=(Button) findViewById(R.id.PtoPButton);
+        Button ButtonPtoN=(Button) findViewById(R.id.PtoNButton);
         if (ButtonPtoC != null) {
             ButtonPtoC.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Intent IntentStartBoard=new Intent(v.getContext(),Net1414080903218BoardActivity.class);
-                    startActivity(IntentStartBoard);
+                    IntentStartBoard=new Intent(v.getContext(),Net1414080903218BoardActivity.class);
+                    IntentStartBoard.putExtra("mode","1");
+                    showChooseDialog();
                 }
             });
         }
+        if (ButtonPtoN != null) {
+            ButtonPtoN.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    IntentStartBoard=new Intent(v.getContext(),Net1414080903218BoardActivity.class);
+                    IntentStartBoard.putExtra("mode","2");
+                    showChooseDialog();
+                }
+            });
+        }
+        if (ButtonPtoP != null) {
+            ButtonPtoP.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    IntentStartBoard=new Intent(v.getContext(),Net1414080903218BoardActivity.class);
+                    IntentStartBoard.putExtra("mode","3");
+                    showChooseDialog();
+                }
+            });
+        }
+    }
+    String[] sizeArry = new String[] { "10 * 10","12 * 12", "15 * 15" };
+
+    /* 选择框 */
+    private void showChooseDialog() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);// 自定义对话框
+        builder.setTitle("选择棋盘大小");
+        builder.setSingleChoiceItems(sizeArry, 0, new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {// which是被选中的位置
+                if(which == 0)size = "10";
+                else if(which == 1)size = "12";
+                else if(which == 2)size = "15";
+                IntentStartBoard.putExtra("size",size);
+                startActivity(IntentStartBoard);
+                //sexView.setText(sexArry[which]);
+                dialog.dismiss();//随便点击一个item消失对话框，不用点击确认取消
+            }
+        });
+        builder.show();// 让弹出框显示
     }
 }
--- AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903218/Net1414080903218BoardActivity.java
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903218/Net1414080903218BoardActivity.java
@@ -11,42 +11,33 @@ import android.widget.Toast;
 public class Net1414080903218BoardActivity extends AppCompatActivity {
     public Board board;
     public Player firstPlayer;
-    public AutoPlayer laterPlayer;
+    public Player laterPlayer;
     private ChessView view;
+    public int mode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_net1414080903218_board);
         start();
-        //Canvas myCanvas = new Canvas();
-        /*DisplayMetrics metric = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(metric);
-        int width = metric.widthPixels;  // 屏幕宽度（像素）
-        int height = metric.heightPixels;  // 屏幕高度（像素）
-        float density = metric.density;  // 屏幕密度（0.75 / 1.0 / 1.5）
-        int densityDpi = metric.densityDpi;  // 屏幕密度DPI（120 / 160 / 240
-    */    //Toast.makeText(this,Integer.valueOf(width)+","+Integer.valueOf(height),Toast.LENGTH_SHORT).show();
     }
     public void start(){
-        board = new Board();
+        int size = Integer.valueOf(getIntent().getStringExtra("size"));
+        board = new Board(size,size);
         firstPlayer = new Player(true,board);
-        laterPlayer = new AutoPlayer(false,board);
+        mode = Integer.valueOf(getIntent().getStringExtra("mode"));
+        if(mode == 1) {
+            laterPlayer = new AutoPlayer(false, board);
+        }
+        else if(mode == 2) {
+            laterPlayer = new NetPlayer(false, board);
+        }
+        else if(mode == 3) {
+            laterPlayer = new Player(false, board);
+        }
+        else laterPlayer = new AutoPlayer(false, board);
         view = (ChessView)findViewById(R.id.ChessView);
+        view.mode = mode;
         view.start(board.getRow(),board.getCol());
     }
-/*    @Override
-    public boolean onTouchEvent(MotionEvent event){
-        float x=event.getX();
-        float y=event.getY();
-        ChessBoard chessBoard=(ChessBoard) findViewById(R.id.ChessBosrd);
-        if(x+chessBoard.grapX/2>chessBoard.boardSX && x-chessBoard.grapX/2<chessBoard.boardEX
-                && y+chessBoard.grapY/2>chessBoard.boardSY && y-chessBoard.grapY/2<chessBoard.boardEY){
-            int row=(int)((x-chessBoard.boardSX+chessBoard.grapY/2)/chessBoard.grapX);
-            int col=(int)((y-chessBoard.boardSY+chessBoard.grapX/2)/chessBoard.grapY);
-            Toast.makeText(chessBoard.getContext(),String.valueOf(row)+"  "+String.valueOf(col),Toast.LENGTH_SHORT).show();
-            chessBoard.drawChess(row,col,Color.BLACK);
-        }
-        return true;
-    }*/
 }
