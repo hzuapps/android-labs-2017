92c4fff273f724c5170b3dde0638b1413ba63d46
LittleUncle <812346412@qq.com>
2017-05-19 15:36:06 +0800

    #4 #142 第四次作业

---
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903135/Net1414080903135ChangeActivity.java
@@ -0,0 +1,13 @@
+package edu.hzuapp.androidlabs.homeworks.net1414080903135;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Net1414080903135ChangeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_net1414080903135_change);
+    }
+}
