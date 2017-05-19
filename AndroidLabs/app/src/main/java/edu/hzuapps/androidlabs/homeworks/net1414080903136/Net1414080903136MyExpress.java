7f72b8e46b20feaa8f996a8a437ed79bed37e3e6
xiwu zhong <xiwu zhong>
2017-05-19 11:08:44 +0800

    #4 #90 第四次作业

---
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903136/Net1414080903136MyExpress.java
@@ -0,0 +1,22 @@
+package edu.hzuapps.androidlabs.homworks.net1414080903136;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.hzuapps.androidlabs.R;
+
+/**
+ * Created by xx on 2017/5/15.
+ */
+
+public class Net1414080903136MyExpress extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab3_net1414080903136,container,false);
+    }
+}
