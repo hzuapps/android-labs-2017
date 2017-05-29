3f8aab8d22bc9076f547c0d453413d1ea3484018
xiang <xiang>
2017-05-29 22:21:35 +0800

    #5 #67 第五次作业

--- AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903108/Net1414080903108Activity.java
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903108/Net1414080903108Activity.java
@@ -1,17 +1,25 @@
 ﻿package edu.hzuapps.androidlabs.homeworks.net1414080903108;
 
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Matrix;
-import android.graphics.Paint;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.ImageView;
+        import android.content.Intent;
+        import android.graphics.Bitmap;
+        import android.graphics.Canvas;
+        import android.graphics.Color;
+        import android.graphics.Matrix;
+        import android.graphics.Paint;
+        import android.net.Uri;
+        import android.os.Environment;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.MotionEvent;
+        import android.view.View;
+        import android.widget.ImageView;
+        import android.widget.Toast;
 
-import com.example.myapplication.R;
+        import com.example.myapplication.R;
+
+        import java.io.File;
+        import java.io.FileOutputStream;
+        import java.io.OutputStream;
 
 public class Net1414080903108Activity extends AppCompatActivity {
     private ImageView iv;
@@ -21,21 +29,23 @@ public class Net1414080903108Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_net1414080903108);
+
         this.iv = (ImageView) this.findViewById(R.id.iv);
         // 创建一张空白图片
-        baseBitmap = Bitmap.createBitmap(480, 640, Bitmap.Config.ARGB_8888);
+        baseBitmap = Bitmap.createBitmap(1200, 1600, Bitmap.Config.ARGB_8888);
         // 创建一张画布
         canvas = new Canvas(baseBitmap);
         // 画布背景为白色
-        canvas.drawColor(Color.WHITE );
+        canvas.drawColor(Color.WHITE);
         // 创建画笔
         paint = new Paint();
         // 画笔颜色为黑色
-        paint.setColor(Color.BLACK );
+        paint.setColor(Color.BLACK);
         // 宽度5个像素
-        paint.setStrokeWidth(8);
+        paint.setStrokeWidth(10);
         // 先将白色背景画上
         canvas.drawBitmap(baseBitmap, new Matrix(), paint);
         iv.setImageBitmap(baseBitmap);
@@ -67,5 +77,24 @@ public class Net1414080903108Activity extends AppCompatActivity {
                 return true;
             }
         });
+    }
+
+    public void save(View view) {
+        try {
+            File file = new File(Environment.getExternalStorageDirectory(),
+                    System.currentTimeMillis() + ".jpg");
+            OutputStream stream = new FileOutputStream(file);
+            baseBitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+            stream.close();
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_MEDIA_MOUNTED);
+            intent.setData(Uri.fromFile(Environment
+                    .getExternalStorageDirectory()));
+            sendBroadcast(intent);
+        } catch (Exception e) {
+            Toast.makeText(this, "保存图片成功", 0).show();
+
+            e.printStackTrace();
+        }
     }
 }
