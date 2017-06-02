aa76fd730c31ce5c24731254cf06a6da512700ef
pengwx <460816663@qq.com>
2017-06-02 23:39:23 +0800

    #5 #63 第五次作业

---
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903224/N1414080903224MyHelper.java
@@ -0,0 +1,23 @@
+//创建数据库的类
+package edu.hzuapps.androidlabs.homeworks.net1414080903224;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.Settings;
+
+/**
+ * Created by peng on 2017/6/1.
+ */
+public class N1414080903224MyHelper  extends SQLiteOpenHelper{
+    public  N1414080903224MyHelper(Context context){
+        super (context,"item.db",null,2);  // 创建数据库，数据库名为 item
+    }
+    public void onCreate(SQLiteDatabase db){
+        System.out.println("onCreate");
+        db.execSQL("CREATE TABLE address(_id INTEGER KEY AUTOINCREMENT , url Varchar(20))") ; //创建address 数据表，字段名为 id,url
+    }
+    public  void onUpgrade(SQLiteDatabase db,int oldVersion,int newVersion){
+        System.out.println("onUpgrade");
+    }
+}
--- AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903224/Net1414080903224Internet.java
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903224/Net1414080903224Internet.java
@@ -1,31 +1,48 @@
 package edu.hzuapps.androidlabs.homeworks.net1414080903224;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.EditText;
+import android.widget.ListView;
+import java.io.FileOutputStream;
 public class Net1414080903224Internet extends AppCompatActivity {
     private Button b_history;
+    private Button b_search;
+    private EditText e_input;
+    private  N1414080903224Dao dao;  //数据库操作类
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_net1414080903224_internet);
+        e_input=(EditText) findViewById(R.id.e_input);
         b_history = (Button) findViewById(R.id.button1);
+        b_search = (Button) findViewById(R.id.button);
+        b_history.setOnClickListener(new ButtonListener());
+        b_search.setOnClickListener(new ButtonListener());
+        dao=new N1414080903224Dao(this);
     }
-        public  void onClick (View v) {
-            switch ( v.getId()){
+
+    private class ButtonListener implements View.OnClickListener {
+        public void onClick(View v) {
+            switch (v.getId()) {
                 case R.id.button1:
-                    Intent intent = new Intent(this,N1414080903224history.class);
+                    Intent intent = new Intent(Net1414080903224Internet.this, N1414080903224history.class);
                     startActivity(intent);
                     break;
             }
-            switch ( v.getId()){
+            switch (v.getId()) {
                 case R.id.button:
-                    Intent intent = new Intent(this,N1414080903224page.class);
+                    Intent intent = new Intent(Net1414080903224Internet.this, N1414080903224page.class);
                     startActivity(intent);
+                    String url=e_input.getText().toString().trim(); //获取用户输入的信息
+                    N1414080903224Address a=new N1414080903224Address(url);
+                    dao.insert(a);  //插入数据库
                     break;
             }
         }
     }
-
+}
