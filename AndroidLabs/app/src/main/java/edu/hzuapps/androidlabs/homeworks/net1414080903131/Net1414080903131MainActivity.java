58f12f3de42de4b4fbc59193b22a9f9dfc906fff
unknown <@PC-20160115CMBC.hzucs.cn>
2017-06-11 23:00:50 +0800

    #6 #154 第六次作业

--- AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903131/Net1414080903131MainActivity.java
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903131/Net1414080903131MainActivity.java
@@ -1,7 +1,11 @@
 package edu.hzuapps.androidlabs.homeworks.net1414080903131;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -12,6 +16,7 @@ public class Net1414080903131MainActivity extends AppCompatActivity {
 
     Button btLogin;
     Button btRegister;
+    Button btJson;
 
     EditText etUsername;
     EditText etPassword;
@@ -22,16 +27,30 @@ public class Net1414080903131MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_net1414080903131_main);
         btLogin= (Button) findViewById(R.id.bt_login);
         btRegister= (Button) findViewById(R.id.bt_register);
+        btJson= (Button) findViewById(R.id.bt_json);
 
         etUsername= (EditText) findViewById(R.id.et_id);
         etPassword= (EditText) findViewById(R.id.et_password);
 
         String username=etUsername.getText().toString();
         String password=etPassword.getText().toString();
+        if (check(username,password)){
+            startActivity(new Intent(Net1414080903131MainActivity.this,Net1414080903131MenuActivity.class));
+        }
+
+        btJson.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Net1414080903131MainActivity.this,Net1414080903131ShowJsonActivity.class));
+            }
+        });
 
 
+    }
 
-
-
+    public boolean check(String name,String password){
+        if (name.equals("linzihong")&&password.equals("123456")){
+            return true;
+        }return false;
     }
 }
