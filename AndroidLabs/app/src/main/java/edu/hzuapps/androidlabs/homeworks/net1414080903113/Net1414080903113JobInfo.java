87a6a54957a0dc3c933eaae7d7e9bb3b4ffba660
zhouchenghai <2584061172@qq.com>
2017-06-17 23:50:59 +0800

    #5 #42 第五次作业

---
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903113/Net1414080903113JobInfo.java
@@ -0,0 +1,47 @@
+package edu.androidlabs.homeworks.Net1414080903113;
+
+/**
+ * Created by Administrator on 2017/6/16.
+ */
+
+public class Net1414080903113JobInfo {
+
+    String name;
+    String location;
+    String salary;
+    String occurTime;
+
+
+
+    public String getOccurTime() {
+        return occurTime;
+    }
+
+    public void setOccurTime(String occurTime) {
+        this.occurTime = occurTime;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
