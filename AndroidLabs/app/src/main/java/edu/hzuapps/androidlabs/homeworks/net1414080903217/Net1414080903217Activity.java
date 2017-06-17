cd813a561fef40f2971e20b6d37613537aeed561
net217ptz <254093288@qq.com>
2017-06-17 22:54:09 +0800

    #6 #106 第六次实验

--- AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903217/Net1414080903217Activity.java
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903217/Net1414080903217Activity.java
@@ -1,8 +1,12 @@
 package edu.hzuapps.androidlabs.homeworks.net1414080903217;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -10,24 +14,32 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import edu.hzuapps.androidlabs.homeworks.net1414080903217.Bean.Mypicture;
+import edu.hzuapps.androidlabs.homeworks.net1414080903217.dao.PictureDao;
 
 public class Net1414080903217Activity extends AppCompatActivity {
 private ListView listView;
     protected void onCreate(Bundle savedInstanceState) {
-
+        FloatingActionButton b1= (FloatingActionButton) findViewById(R.id.fab);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_load_picture);
+        setContentView(R.layout.activity_load_picture_net1414080903217);
         MyListView();
-            }
 
 
 
+    b1.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(Net1414080903217Activity.this,Net1414080903217_xml.class);
+            startActivity(intent);
+        }
+    });}
     public void MyListView(){
         final String[] strings = new String[]{
                 "图片1",
                 "图片2",
                 "图片3",
-                "图片4",
+                "图片4"
 
         };
         int[] imgId = new int[]{
@@ -45,7 +57,10 @@ private ListView listView;
             listItem.put("text",strings[i]);
             listItems.add(listItem);
         }
-        SimpleAdapter mSimpleAdapter = new SimpleAdapter(this,listItems,R.layout.activity_load_picture,new String[]{"img","text"},new int[]{R.id.image_view,R.id.text_view});
+        SimpleAdapter mSimpleAdapter = new SimpleAdapter(this,listItems,R.layout.content_load_picture_net1414080903217,new String[]{"img","text"},new int[]{R.id.image_view,R.id.text_view});
         listView.setAdapter(mSimpleAdapter);
+
+
+            }
+
     }
-}
---
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903217/Net1414080903217_xml.java
@@ -0,0 +1,71 @@
+﻿package edu.hzuapps.androidlabs.homeworks.net1414080903217;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import java.io.IOException;
+
+public class Net1414080903217_xml extends AppCompatActivity {
+
+    private Handler handler=null;
+    private TextView textView=null;
+    private String cont="";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.get_xml_net1414080903217);
+        handler=new Handler();
+
+        Button button = (Button) this.findViewById(R.id.btn_1);
+        textView = (TextView) this.findViewById(R.id.content);
+    }
+        
+        public void Click(View v) {
+
+            new Thread(){
+                public void run(){
+                    try {
+                        Document doc = Jsoup.connect("https://github.com/pengtianzhu/android-labs-2017/blob/master/1414080903217.xml").get();
+                        int i = 1;
+                        Elements data = doc.select("#LC"+i);
+                        while(!data.isEmpty()){
+                            cont += data.text();
+                            i++;
+                            data = doc.select("#LC"+i);
+                        }
+
+                        Log.d("test",cont);
+
+                    }catch(IOException e){
+                        System.out.println("" + e);
+                    }
+
+                    handler.post(runnableUi);
+                }
+            }.start();
+        }
+
+
+
+
+
+    Runnable   runnableUi=new  Runnable(){
+        @Override
+        public void run() {
+            textView.setText(cont);
+
+        }
+
+    };
+
+
+
+
+}
