99928b6a702da80e6ff7721a5cb6ba76833c972d
IcyvE <l>
2017-06-27 10:52:29 +0800

    #6 #134 第六次作业

--- AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903126/Net1414080903126_initialAct1.java
+++ AndroidLabs/app/src/main/java/edu/hzuapps/androidlabs/homeworks/net1414080903126/Net1414080903126_initialAct1.java
@@ -24,16 +24,16 @@ public class Net1414080903126_initialAct1 extends AppCompatActivity {
     public void click(View view){
         switch (view.getId()){
             case R.id.btn_2:
-                Intent intent2=new Intent(this,Net1414080903126_Act3.class);
+                Intent intent2=new Intent(this,Net1414080903126_Act4.class);
                 startActivity(intent2);
-                break;
-            case R.id.btn_3:
-                Intent intent3=new Intent(this,Net1414080903126_Act4.class);
-                startActivity(intent3);
                 break;
             case R.id.btn_1:
                 Intent intent1=new Intent(this,Net1414080903126_Act2.class);
                 startActivity(intent1);
+                break;
+            case R.id.btn_3:
+                Intent intent3=new Intent(this,Net1414080903126_Js.class);
+                startActivity(intent3);
                 break;
         }
 
